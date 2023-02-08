8f84b356415e4904eb5ae389de0d2ff2b9a14016
BenPerraud <benjaminperraud@outlook.fr>
2023-02-08 16:51:52 -0400

    version_filtres_auto_ok

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -2,8 +2,9 @@
 
 /* Application du code
 ---------------------------------------------------------------------------*/
-categoryAPI () //On affiche les filtres
-    .then(r => createButton(r))
+categoryAPI () // On affiche les filtres
+    .then(r => categoryArray(r)) // On crée un tableau de catégorie (id + name ) + catégorie "Tous"
+    .then(r => createButton(r)) // On crée les boutons HTML avec le filtre sur le click
 
 worksAPI () // On affiche les éléments de la requête worksAPI avec la fonction createDOM
     .then (r => createDom(r))
@@ -70,51 +71,23 @@ async function categoryAPI () {
    
 }
 
-categoryAPI ()
-    .then(r => console.log(r))
 
-/*const all = {
-        id: 0,
-        name: "Tous"
+// On crée une fonction pour créer un tableau de catégorie + Tous (pour faire tous les boutons)
+function categoryArray (x) {
+    const categoryArrayEmpty = []
+    const all = { id: 0, name: "Tous"} // On rajoute le cas "Tous", non présent dans l'API
+    categoryArrayEmpty.push(all)
+    for (let i in x) {
+        categoryArrayEmpty.push(x[i])
     }
-    const result = new Set (response)
-    return result.add(all)*/
-
-
-
-
-
-
-
-
-
-
+    return categoryArrayEmpty
+}
 
 
 // On crée une fonction qui supprime la galerie en cours
 function cleanGallery () {
     document.querySelector(".galleryJS").innerHTML = ""
 }
-
-
-
-// On crée une fonction pour créer les boutons sur le HTML depuis les catégories de l'API
-function createButton (x) {
-    for (let i in x) {
-        let button = document.createElement("button") // On crée un élément Bouton
-        button.appendChild(document.createTextNode(x[i].name)) // On incorpore le texte de chaque bouton avec le name de la requête categoriesAPI
-        button.className = "filterbuttonDetails" // On rattache le bouton à sa classe CSS
-        button.setAttribute ("type", "button")
-        button.id = x[i].id
-        filterButton.appendChild(button) // On rattache le bouton à l'élément parent filterButton (id)
-        button.addEventListener("click", function () {
-            cleanGallery ()
-            filter(x[i].name)
-        })
-    }
-}
-
-
 
 
 // On crée une fonction filtre par catégorie
@@ -125,23 +98,24 @@ async function filter (y) {
     }
 
 
-/*
-function domCategory (category) {
-    let btn = document.createElement("button")
-    btn.appendChild(document.createTextNode(category.name))
-    btn.className = "filterbuttonDetails"
-    btn.setAttribute ("type", "button")
-    btn.id = category.id
-    btn.addEventListener("click", function () {
-        filter(parseInt(category.id))
-
-    })
-    filterButton.appendChild(btn)
+// On crée une fonction pour créer les boutons sur le HTML depuis les catégories de l'API (+ bouton "Tous")
+function createButton (x) {
+    for (let i in x) {
+        let button = document.createElement("button") // On crée un élément Bouton
+        button.appendChild(document.createTextNode(x[i].name)) // On incorpore le texte de chaque bouton avec le name de la requête categoriesAPI
+        button.className = "filterbuttonDetails" // On rattache le bouton à sa classe CSS
+        button.setAttribute ("type", "button")
+        button.id = x[i].id
+        filterButton.appendChild(button) // On rattache le bouton à l'élément parent filterButton (id)
+        button.addEventListener("click", function () {
+            cleanGallery ()
+            if (x[i].id === 0) {
+                worksAPI ()
+                    .then(r => createDom(r))
+            } else {
+                filter(x[i].name)}
+        })
+    }
 }
 
-const filterButton = document.getElementById("filterButton")
 
-
-document.getElementById("all").addEventListener("click", function () {
-    filter(0)
-}) */
